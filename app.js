7735cf5daa451a7455cb14bf9315ad8bf19798d4
Rasulyo <kkg.rasul@gmail.com>
2024-07-17 23:19:48 +0600

    gift, partnership

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
   console.log('fff');
+  document.querySelectorAll('.faq-sidebar a').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      // Remove 'active' class from all faq-items
+      document.querySelectorAll('.faq-item').forEach((item) => {
+        item.classList.remove('active');
+      });
+
+      // Add 'active' class to the clicked item
+      const target = document.querySelector(this.getAttribute('href'));
+      console.log(target, 'target before');
+      target.classList.add('active');
+      console.log(target, 'target after');
+
+      // Scroll into view
+      target.scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
 });
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
